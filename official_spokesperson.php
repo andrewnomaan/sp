4ab9c8439dcf1045dcaf6e0c0666bda1f25c7265
Andrewnomaan <andrew.nomaan@gmail.com>
2021-11-30 18:08:16 +0530

    official

---
+++ official_spokesperson.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>सरकारी प्रवक्ता - समाजवादी पार्टी, बिजनौर</title>
+<?php include 'header.php'; 
+  $assets='assets/img/';
+?>
+<main id="main">
+    <section id="" class="section-bg">
+      <div class="container-fluid">
+
+        <div class="section-title">
+            <h2>सरकारी प्रवक्ता</h2>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-sm-10 col-12">
+        <table class="table table-bordered text-center">
+    <thead>
+    <tr>
+      <th scope="col" >No.</th>
+      <th scope="col" >Image</th>
+      <th scope="col" >Name</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="table-hover">
+      <th scope="row">1</th>
+      <td class="spoke_person_img "><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>राजीव राय </strong>( +91- 9844017728)</td>
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">2</th>
+      <td class="spoke_person_img"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>जूही सिंह </strong>( +91- 9793083339)</td>
+     
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">3</th>
+      <td class="spoke_person_img"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>नावेद सिद्दीकी </strong>( +91- 9839065380  )</td>
+     
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">4</th>
+      <td class="spoke_person_img"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>जगदेव सिंह यादव </strong>( +91- 9938928063 )</td>
+     
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">5</th>
+      <td class="spoke_person_img"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>उदय वीर सिंह </strong>( +91- 9412750002 )</td>
+     
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">6</th>
+      <td class="spoke_person_img"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>घनश्याम तिवारी </strong>( +91- 9334186699 )</td>
+    
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">7</th>
+      <td class="spoke_person_img"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>सुनील सिंह यादव </strong>( +91- 9415785049  )</td>
+     
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">8</th>
+      <td class="spoke_person_img"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>संजय लथार</strong>( +91- 9219535799  )</td>
+     
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">9</th>
+      <td class="spoke_person_img"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>सैयद अब्बास अली </strong>( +91- 9415906609 )</td>
+    
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">10</th>
+      <td class="spoke_person_img"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>वंदना सिंह </strong>( +91- 9811156930 )</td>
+     
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">11</th>
+      <td class="spoke_person_img"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>कश्यप </strong>( +91- 9415419867 )</td>
+     
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">12</th>
+      <td class="spoke_person_img"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>शिवेंद्र विक्रम सिंह </strong>( +91-9958415550 )</td>
+    
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">13</th>
+      <td class="spoke_person_img"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>नासिर सलीम </strong>( +91- 9810193593 )</td>
+    
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">14</th>
+      <td class="spoke_person_img"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>अनुराग भदौरिया </strong>( +91- 9868150600 )</td>
+    
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">15</th>
+      <td class="spoke_person_img"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>अब्दुल हफीज गांधी </strong>( +91- 8057959634 )</td>
+     
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">16</th>
+      <td class="spoke_person_img"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>पवन पांडेय </strong>( +91- 9415022313 )</td>
+     
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">17</th>
+      <td class="spoke_person_img"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>अली खान महमूदाबाद </strong>( +91- 9810806966 )</td>
+     
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">18</th>
+      <td class="spoke_person_img"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>निधि यादव </strong>( +91- 9919177174  )</td>
+      
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">19</th>
+      <td class="spoke_person_img text-center"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>राजकुमार भाटी </strong>( +91- 9313203731 )</td>
+     
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">20</th>
+      <td class="spoke_person_img text-center"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>ऋचा सिंह </strong>( +91- 9415580935 )</td>
+      
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">21</th>
+      <td class="spoke_person_img text-center"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>मनोज राय सुखचंडी </strong>( +91- 9415202225 )</td>
+     
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">22</th>
+      <td class="spoke_person_img text-center"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>जितेंद्र उर्फ ​​जीतू वर्मा </strong>( +91- 9415182523 )</td>
+     
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">23</th>
+      <td class="spoke_person_img text-center"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>फैजान अली किदवई  </strong>( +91- 9821098807)</td>
+     
+    </tr>
+    <tr class="table-hover">
+      <th scope="row">24</th>
+      <td class="spoke_person_img text-center"><img src="<?php echo $assets ?>logo.jpg" class="img-fuid" alt=""></td>
+      <td > <strong>राम प्रताप सिंह </strong>( +91- 9997066002 )</td>
+      
+    </tr>
+    
+  </tbody>
+</table>
+</div>
+        </div>
+
+      </div>
+    </section>
+</main>
+<?php include('footer.php') ?>
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- JS Files -->
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+  <!-- Main JS File -->
+  <script src="assets/js/main.js"></script>
+    
+</body>
+</html>
