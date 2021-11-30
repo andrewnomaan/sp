4f0ce2eb41ad3c6cf2ba5a015b88d3012cfe755e
Andrewnomaan <andrew.nomaan@gmail.com>
2021-11-30 18:08:43 +0530

    gallery

---
+++ photo_gallery.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>फोटो गैलरी - समाजवादी पार्टी, बिजनौर</title>
+    <?php include 'header.php'; 
+  $assets='assets/img/downloads/';
+?>
+    <main id="main">
+        <section id="gallery" class="section-bg">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>फोटो गैलरी</h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6">
+                      <img src="assets/img/1 .jpg" alt="No image" class="img-fluid">
+                    </div>
+                    <div class="col-md-6">
+                      <img src="assets/img/2.jpg" alt="No image" class="img-fluid">
+                    </div>
+                    <div class="col-md-6 pt-4">
+                      <img src="assets/img/3.jpg" alt="No image" class="img-fluid">
+                    </div>
+                    <div class="col-md-6 pt-4">
+                      <img src="assets/img/4.jpg" alt="No image" class="img-fluid">
+                    </div>
+                    <div class="col-md-6 pt-4">
+                      <img src="assets/img/5.jpg" alt="No image" class="img-fluid">
+                    </div>
+                    <div class="col-md-6 pt-4">
+                      <img src="assets/img/6.jpg" alt="No image" class="img-fluid">
+                    </div>
+                    <div class="col-md-6 pt-4">
+                      <img src="assets/img/7.jpg" alt="No image" class="img-fluid">
+                    </div>
+                    <div class="col-md-6 pt-4">
+                      <img src="assets/img/8.jpg" alt="No image" class="img-fluid">
+                    </div>
+                    
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php include('footer.php') ?>
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- JS Files -->
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+    </body>
+
+</html>
