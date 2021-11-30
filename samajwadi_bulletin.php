58029b9215112fc2fe799b41f13b9032bd5300e0
Andrewnomaan <andrew.nomaan@gmail.com>
2021-11-30 18:09:39 +0530

    bulletin

---
+++ samajwadi_bulletin.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>समाजवादी बुलेटिन - समाजवादी पार्टी, बिजनौर</title>
+<?php include 'header.php'; 
+  $assets='assets/img/downloads/';
+?>
+<main id="main">
+    <section id="" class="section-bg">
+      <div class="container-fluid">
+
+        <div class="section-title">
+            <h2>समाजवादी बुलेटिन</h2>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-sm-10 table-responsive col-12">
+        <table class="table table-bordered">
+    <thead>
+    <tr>
+      <th scope="col" class="ps-3">No.</th>
+      <th scope="col">Month</th>
+      <th scope="col">Year</th>
+      <th scope="col" class="text-center">View/Download</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="table-hover">
+      <th scope="row" class="ps-3">1</th>
+      <td>November</td>
+      <td>2021</td>
+      <td class="text-center"><a href="<?php echo $assets ?>1638255204_Samajwadi Bulletin November-2021.pdf" class="bi bi-download"></a></td>
+    </tr>
+    <tr class="table-hover">
+      <th scope="row" class="ps-3">2</th>
+      <td>October</td>
+      <td>2021</td>
+      <td class="text-center"><a href="<?php echo $assets ?>1635783398_Samajwadi-Bulletin-October2021.pdf" class="bi bi-download"></a></td>
+    </tr>
+    <tr class="table-hover">
+      <th scope="row" class="ps-3">3</th>
+      <td>September</td>
+      <td>2021</td>
+      <td class="text-center"><a href="<?php echo $assets ?>1633062353_SamajwadiBulletin-Sept-2021.pdf" class="bi bi-download"></a></td>
+    </tr>
+    <tr class="table-hover">
+      <th scope="row" class="ps-3">4</th>
+      <td>August</td>
+      <td>2021</td>
+      <td class="text-center"><a href="<?php echo $assets ?>1630763593_Samajwadi-Bulletin-August-2021.pdf" class="bi bi-download"></a></td>
+    </tr>
+    <tr class="table-hover">
+      <th scope="row" class="ps-3">5</th>
+      <td>July</td>
+      <td>2021</td>
+      <td class="text-center"><a href="<?php echo $assets ?>1628065546_Samajwadi Bulletin July 2021-j.pdf" class="bi bi-download"></a></td>
+    </tr>
+    <tr class="table-hover">
+      <th scope="row" class="ps-3">6</th>
+      <td>June</td>
+      <td>2021</td>
+      <td class="text-center"><a href="<?php echo $assets ?>1624961728_sSamajwadi-Bulletin-June-2021.pdf" class="bi bi-download"></a></td>
+    </tr>
+    <tr class="table-hover">
+      <th scope="row" class="ps-3">7</th>
+      <td>May</td>
+      <td>2021</td>
+      <td class="text-center"><a href="<?php echo $assets ?>1622885918_Samajwadi-Bulletin-May2021.pdf" class="bi bi-download"></a></td>
+    </tr>
+    <tr class="table-hover">
+      <th scope="row" class="ps-3">8</th>
+      <td>April</td>
+      <td>2021</td>
+      <td class="text-center"><a href="<?php echo $assets ?>1622885873_samajwadi-bulletin-April-2021.pdf" class="bi bi-download"></a></td>
+    </tr>
+    <tr class="table-hover">
+      <th scope="row" class="ps-3">9</th>
+      <td>March</td>
+      <td>2021</td>
+      <td class="text-center"><a href="<?php echo $assets ?>1622885830_Samajwadi-Bulletin-MARCH-2021.pdf" class="bi bi-download"></a></td>
+    </tr>
+    <tr class="table-hover">
+      <th scope="row" class="ps-3">10</th>
+      <td>February</td>
+      <td>2021</td>
+      <td class="text-center"><a href="<?php echo $assets?>1622885762_sp-Bulletin-Feb2021.pdf" class="bi bi-download"></a></td>
+    </tr>
+    <tr class="table-hover">
+      <th scope="row" class="ps-3">11</th>
+      <td>January</td>
+      <td>2021</td>
+      <td class="text-center"><a href="<?php echo $assets ?>1622884583_Samajwadi-Bulletin-January2021.pdf" class="bi bi-download"></a></td>
+    </tr>
+   
+  </tbody>
+</table>
+</div>
+        </div>
+
+      </div>
+    </section>
+</main>
+<?php include('footer.php') ?>
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- JS Files -->
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+  <!-- Main JS File -->
+  <script src="assets/js/main.js"></script>
+    
+</body>
+</html>
