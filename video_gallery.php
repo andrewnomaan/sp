6094b9585afec77d29adee5eafbd9bc1506c3a2b
Andrewnomaan <andrew.nomaan@gmail.com>
2021-12-01 18:03:08 +0530

    videogallery

---
+++ video_gallery.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>वीडियो गैलरी - समाजवादी पार्टी, बिजनौर</title>
+    <?php include 'header.php'; 
+  $assets='assets/img/downloads/';
+?>
+    <style>
+    h5 a:hover {
+        cursor: default !important;
+        color: #e40000 !important;
+    }
+    </style>
+    <main id="main">
+        <section id="gallery" class="section-bg">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>वीडियो गैलरी</h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4 mb-4">
+                        <iframe  src="https://www.youtube.com/embed/mTGzeyzDQ3s"
+                            frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="" width="100%"></iframe>
+                        <h5 class="card-title pt-2"> <a href>2017 के चुनाव के लिए अभियान</a></h5>
+                    </div>
+                    <div class="col-md-4 mb-4">
+                        <iframe  src="https://www.youtube.com/embed/kg_PALiMjx4"
+                            frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="" width="100%"></iframe>
+                        <h5 class="card-title pt-2"> <a href>समाजवादी विकास रथ यात्रा</a></h5>
+                    </div>
+                    <div class="col-md-4 mb-4">
+                        <iframe src="https://www.youtube.com/embed/JrMR03hjmm0"
+                            frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="" width="100%"></iframe>
+                        <h5 class="card-title pt-2"> <a href>आगरा लखनऊ एक्सप्रेसवे</a></h5>
+                    </div>
+                    <div class="col-md-4 mb-4">
+                    <iframe src="https://www.youtube.com/embed/qL9NM6aJrJ4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="" width="100%"></iframe>
+                        <h5 class="card-title pt-2"> <a href>काम बोलता है वीडियो</a></h5>
+                    </div>
+                    <div class="col-md-4 mb-4">
+                    <iframe src="https://www.youtube.com/embed/XmaaoXqa_YY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="" width="100%"></iframe>
+                        <h5 class="card-title pt-2"> <a href>गोमती रिवर फ्रंट</a></h5>
+                    </div>
+                    <div class="col-md-4 mb-4">
+                    <iframe src="https://www.youtube.com/embed/yfiVpDkP-Is" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="" width="100%"></iframe>
+                        <h5 class="card-title pt-2"> <a href>डायल 100</a></h5>
+                    </div>
+                    <div class="col-md-4 mb-4">
+                    <iframe src="https://www.youtube.com/embed/8xMukCAlZ0M" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="" width="100%"></iframe>
+                        <h5 class="card-title pt-2"> <a href>ताज गंज सौंदर्यीकरण</a></h5>
+                    </div>
+                    <div class="col-md-4 mb-4">
+                    <iframe src="https://www.youtube.com/embed/efJOadF0Ljs" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="" width="100%"></iframe>
+                        <h5 class="card-title pt-2"> <a href>मेट्रो परियोजनाएं</a></h5>
+                    </div>
+                    <div class="col-md-4 mb-4">
+                    <iframe src="https://www.youtube.com/embed/h2Bt6J0W5aE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="" width="100%"></iframe>
+                        <h5 class="card-title pt-2"> <a href>विकास भारती</a></h5>
+                    </div>
+                    <div class="col-md-4 mb-4">
+                    <iframe src="https://www.youtube.com/embed/tN-JuRHKe2c" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="" width="100%"></iframe>
+                        <h5 class="card-title pt-2"> <a href>हुसैनाबाद हेरिटेज वॉक</a></h5>
+                    </div>
+                    <div class="col-md-4 mb-4">
+                    <iframe src="https://www.youtube.com/embed/S70BpPhtSDc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="" width="100%"></iframe>
+                        <h5 class="card-title pt-2"> <a href>नोएडा-#काम बोलता है</a></h5>
+                    </div>
+                    <div class="col-md-4 mb-4">
+                    <iframe src="https://www.youtube.com/embed/1WA_swyRLSM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="" width="100%"></iframe>
+                        <h5 class="card-title pt-2"> <a href>समाजवादी पार्टी : महिला - अखिलेश यादव</a></h5>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php include('footer.php') ?>
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- JS Files -->
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+    </body>
+
+</html>
