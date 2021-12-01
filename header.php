d4519b13ebaa8accc3bed6439c57db497fdb0fa7
Andrewnomaan <andrew.nomaan@gmail.com>
2021-12-01 18:05:01 +0530

    header

---
+++ header.php
@@ -0,0 +1,96 @@
+  <!-- Favicons -->
+  <link href="assets/img/logo.JPG" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+ 
+ <!-- ======= Top Bar ======= -->
+ <section id="topbar" class="d-flex align-items-center">
+   <div class="container d-flex justify-content-center justify-content-md-between">
+     <div class="contact-info d-flex align-items-center">
+       <i class="bi bi-envelope d-flex align-items-center d-none d-md-block"><a href="mailto:contact@example.com">contact@example.com</a></i>
+       <i class="bi bi-phone d-flex align-items-center ms-4 d-none d-md-block"><span>+91 0000 0000 00</span></i>
+     </div>
+     <div class="social-links d-md-flex align-items-center">
+       <a href="https://twitter.com/samajwadibijnor" class="twitter"><i class="bi bi-twitter"></i></a>
+       <a href="https://www.facebook.com/pages/category/Political-Party/Samajwadi-Party-Bijnor-106739211205515" class="facebook"><i class="bi bi-facebook"></i></a>
+     </div>
+   </div>
+ </section>
+
+ <!-- ======= Header ======= -->
+ <header id="header" class="sticky-top header-inner-pages">
+   <div class="container d-flex align-items-center justify-content-between">
+
+     <a href="index.html" class="logo"><img src="assets/img/logo.JPG" alt="" class="img-fluid"></a>
+
+     <nav id="navbar" class="navbar">
+       <ul>
+         <li><a class="nav-link scrollto active" href="./">होम</a></li>
+         <li class="dropdown"><a href="#"><span>बारे में</span> <i class="bi bi-chevron-down"></i></a>
+           <ul>
+             <li><a href="about_party.php">पार्टी के बारे में</a></li>
+             <li><a href="party_history.php">पार्टी का इतिहास</a></li>
+             <li><a href="party_ideology.php">पार्टी विचारधारा</a></li>
+             <li><a href="party_constitution.php">पार्टी संविधान</a></li>
+             <li><a href="party_leader.php">पार्टी नेता</a></li>
+             <li><a href="party_president.php">पार्टी अध्यक्ष</a></li>
+           </ul>
+         </li>
+         <li><a class="nav-link scrollto" href="#portfolio">जिला अध्यक्ष</a></li>
+         <li class="dropdown"><a href="#"><span>सदस्य</span> <i class="bi bi-chevron-down"></i></a>
+           <ul>
+             <li><a href="#">विधान परिषद के सदस्य</a></li>
+             <li><a href="#">राज्यसभा के सदस्य</a></li>
+             <li><a href="#">लोकसभा के सदस्य</a></li>
+             <li><a href="#">विधान सभा के सदस्य</a></li>
+           </ul>
+         </li>
+         <li class="dropdown"><a href="#"><span>विकास पहल</span> <i class="bi bi-chevron-down"></i></a>
+           <ul>
+             <li><a href="#">महिला सशक्तिकरण की पहल</a></li>
+             <li><a href="#">बुनियादी ढांचा पहल</a></li>
+             <li><a href="#">कृषि पहल</a></li>
+             <li><a href="#">व्यापार पहल</a></li>
+             <li><a href="#">औद्योगिक पहल</a></li>
+             <li><a href="#">शिक्षा पहल</a></li>
+           </ul>
+         </li>
+         <li class="dropdown"><a href="#"><span>मीडिया</span> <i class="bi bi-chevron-down"></i></a>
+           <ul>
+             <li><a href="press_vigyapti.php">प्रेस विज्ञप्ति</a></li>
+             <li><a href="samajwadi_bulletin.php">समाजवादी बुलेटिन</a></li>
+             <li><a href="#">समाचार</a></li>
+             <li><a href="photo_gallery.php">फोटो गैलरी</a></li>
+             <li><a href="video_gallery.php">वीडियो गैलरी</a></li>
+           </ul>
+         </li>
+         <li class="dropdown"><a href=""><span>पार्टी में शामिल हों</span> <i class="bi bi-chevron-down"></i></a>
+           <ul>
+             <li><a href="jointwithus.php">सदस्य बनें</a></li>
+           </ul>
+         </li>
+         <li><a class="nav-link scrollto" href="contact.php">संपर्क</a></li>
+       </ul>
+       <i class="bi bi-list mobile-nav-toggle"></i>
+     </nav>
+     <!-- .navbar -->
+   </div>
+ </header>
+ <!-- End Header -->
