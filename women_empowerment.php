625c38332dfc78287fc804b5d3fcec245ac41ca2
Andrewnomaan <andrew.nomaan@gmail.com>
2021-12-03 18:10:26 +0530

    women_empowerment0

---
+++ women_empowerment.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>महिला सशक्तिकरण - समाजवादी पार्टी, बिजनौर</title>
+    <?php include 'header.php'; 
+  $assets='assets/img/';
+?>
+    <main id="main">
+        <section id="" class="section-bg">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>महिला सशक्तिकरण पहल</h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-5">
+                        <img src="assets/img/women_empower.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-lg-7 pt-3">
+                        <p>
+                            समाजवादी पार्टी ने हमेशा महिला सशक्तिकरण को अत्यधिक महत्व दिया है और उनकी शिक्षा, आर्थिक
+                            स्वतंत्रता, स्वास्थ्य और समग्र कल्याण के लिए कई योजनाएं शुरू की हैं। जिन महिलाओं को मौद्रिक
+                            सहायता की आवश्यकता है, उनकी मदद के लिए रानी लक्ष्मी बाई महिला अधिकारिता कोष की स्थापना से
+                            लेकर अंतरराष्ट्रीय स्तर पर प्रशंसित 1090 महिला पावर लाइन तक, जिसने महिलाओं द्वारा रिपोर्ट
+                            किए गए 5,58,000 मामलों में तेजी लाई है। समाजवादी पार्टी समाजवादी समाज बनाने में विश्वास रखती
+                            है,
+</p><p>
+                            जो सभी के लिए समानता के सिद्धांत पर काम करता है। समाजवादी पार्टी द्वारा महिलाओं को मजबूत
+                            बनाने और उनके सर्वांगीण विकास को बढ़ावा देने के उद्देश्य से कई स्वास्थ्य योजनाएं शुरू की गई
+                            हैं। उदाहरण के लिए, किशोरी शक्ति योजना; 840 महिलाओं और बच्चों के लिए लागू, यह उन्हें
+                            व्यावसायिक प्रशिक्षण और पौष्टिक भोजन प्रदान करता है।
+</p><p>
+                            महिलाएं सभी क्षेत्रों में पुरुषों के समान प्रगति चाहती हैं, जैसे समान काम के लिए समान वेतन।
+                            पिछले कुछ वर्षों में महिलाओं की स्थिति और स्थिति अविश्वसनीय रूप से उत्तर प्रदेश में उनके
+                            सशक्तिकरण पर केंद्रित
+                            
+                        </p>
+                    </div>
+                    <div class="col-12 pt-2">
+                        <p>
+                         कई योजनाओं के कारण बढ़ी है। उदाहरण के लिए, कन्या विद्याधन युवा
+                            लड़कियों को शिक्षा प्राप्त करने के लिए प्रोत्साहित करता है और राज्य के नए आईटी शहर में
+                            महिलाओं के लिए 50% नौकरियां आरक्षित होंगी।
+                        </p>
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/women_empower1.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/women_empower2.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/women_empower3.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/women_empower4.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/women_empower5.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/women_empower6.jpg" alt="" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php include('footer.php') ?>
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- JS Files -->
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+    </body>
+
+</html>
