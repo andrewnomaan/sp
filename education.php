5df5f1fc5f76cdb96f57637c944feda7076bee1c
Andrewnomaan <andrew.nomaan@gmail.com>
2021-12-03 18:15:39 +0530

    education

---
+++ education.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>शिक्षा पहल - समाजवादी पार्टी, बिजनौर</title>
+    <?php include 'header.php'; 
+?>
+    <main id="main">
+        <section id="" class="section-bg">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>शिक्षा पहल</h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-5">
+                        <img src="assets/img/education.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-lg-7 pt-3">
+                        <p>
+                            यूनिसेफ इंडिया एनुअल स्टेटस ऑफ एजुकेशन रिपोर्ट, 2015 के अनुसार, "भारत के सबसे अधिक आबादी
+                            वाले राज्य उत्तर प्रदेश (यूपी) ने पिछले आधे दशक में प्राथमिक शिक्षा में प्रभावशाली लाभ कमाया
+                            है।"
+</p><p>
+                            उत्तर प्रदेश अब 57% साक्षरता दर और 34,000,000 स्कूल जाने वाले बच्चों का दावा करता है, जो
+                            राज्य के कुल बच्चों का 93% है। समाजवादी पार्टी दुनिया को अधिक शिक्षित बनाने पर ध्यान
+                            केंद्रित करती है और सभी को शिक्षित करने के लिए कई योजनाएं शुरू की हैं। सर्वो पर दृढ़ता से
+                            पालन करने से
+</p><p>
+                            शिक्षा अभियान और बजट रु. अल्पसंख्यकों और विकलांग लोगों के लिए विभिन्न शैक्षिक योजनाओं को
+                            चलाने के लिए 15,397 करोड़ रुपये, प्रारंभिक शिक्षा को उन्नत करने, ओबीसी छात्रों के लिए पोस्ट
+                            मैट्रिक छात्रवृत्ति की पेशकश और बहुत कुछ, समाजवादी पार्टी ने साबित कर दिया है कि वह अपने
+                            लक्ष्यों की प्राप्ति की दिशा में लगातार काम करना जारी रखेगी।
+</p><p>
+                            समाजवादी पार्टी द्वारा लड़कियों के लिए कुछ विशेष शैक्षिक योजनाएं चलाई जाती हैं, जैसे 'कन्या
+                            विद्याधन' और 'हमारी बेटी, उसका कल', जिसमें रुपये की सहायता की आवश्यकता होती है। 10वीं तक
+                            पढ़ने वाली लड़कियों की शिक्षा के लिए 30,000 रुपये।
+
+                           
+                        </p>
+
+                    </div>
+                    <div class="col-12 pt-2">
+                        <p>
+                        उत्तर प्रदेश राज्य के बजट 2016-17 में, लगभग रु। छात्रों के लिए शैक्षणिक संस्थानों, योजनाओं
+                            और व्यावसायिक प्रशिक्षण के लिए 30,000 करोड़ रुपये निर्धारित किए गए हैं। छात्रों को उच्च
+                            शिक्षा प्रदान करने के लिए वर्ष 2016-17 में 20 से अधिक नए केंद्रों पर निर्णय लिया गया है।
+                        </p>
+                     
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/education1.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/education2.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/education3.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/education4.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/education5.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/education6.jpg" alt="" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php include('footer.php') ?>
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- JS Files -->
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+    </body>
+
+</html>
