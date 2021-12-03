d71da729030d69ff7cc445bc52b8007a173e5ca6
Andrewnomaan <andrew.nomaan@gmail.com>
2021-12-03 18:14:26 +0530

    agriculture

---
+++ agriculture.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>कृषि पहल - समाजवादी पार्टी, बिजनौर</title>
+    <?php include 'header.php'; 
+?>
+    <main id="main">
+        <section id="" class="section-bg">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>कृषि पहल</h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-5">
+                        <img src="assets/img/5.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-lg-7 pt-3">
+                        <p>
+                            वर्ष 2016-17 को समाजवादी पार्टी द्वारा किसानों का वर्ष बनाया गया है, जिसने हमेशा किसानों के
+                            विकास को सुनिश्चित किया है क्योंकि यह समझते हैं कि यह राज्य के विकास और विकास के लिए
+                            महत्वपूर्ण है। यूपी बजट 2016-17 में उन्होंने किसानों को रुपये तक की बकाया राशि के भुगतान के
+                            लिए एक विशेष प्रावधान किया है। 1336 करोड़। रु. किसान दुर्घटना बीमा योजना के लिए 240 करोड़
+                            रुपये निर्धारित किए गए हैं। सूखा प्रभावित क्षेत्रों के किसानों को दिया जाएगा
+                        </p>
+                        <p>
+                            राज्य राहत कोष से वित्तीय मदद और बर्बाद उपज के लिए मुआवजा भी दिया जाएगा। समाजवादी पार्टी ने
+                            सभी किसान और कृषि संबंधित विभागों को डिजिटल कर दिया है जो यह सुनिश्चित करने के लिए ऑनलाइन
+                            निगरानी को सक्षम करेगा कि हर एक किसान योजनाओं से लाभान्वित हो। किसानों को उनकी उपज का
+                            सर्वोत्तम मूल्य दिलाने के लिए लखनऊ, सैफई, मैनपुर, बहराइच, कासगंज, झांसी, कन्नौज और हापुर में
+                            किसान बाजार का निर्माण किया जा रहा है. समाजवादी पार्टी के नेतृत्व वाली सरकार का लक्ष्य कुल
+                            रुपये के फसल ऋण वितरित करना है। बीज, उर्वरक और कृषि उपकरण की समय पर खरीद के लिए 93,212 करोड़
+                            रुपये।
+                        </p>
+
+                    </div>
+                    <div class="col-12 pt-2">
+                        <p>
+                            खेतों में पर्यावरण के अनुकूल सौर फोटोवोल्टिक पंप भी लगाए जाएंगे ताकि किसान महंगी डीजल से
+                            उत्पन्न बिजली पर निर्भरता कम कर सकें। युवा किसानों को रोजगार और प्रशिक्षण प्रदान करने के लिए
+                            प्रशिक्षित कृषि उद्यमियों के तहत 1,000 से अधिक कृषि-जंक्शन स्थापित किए जा रहे हैं। ये सभी
+                            कदम उत्तर प्रदेश के समग्र विकास को सुनिश्चित करने के लिए उठाए जा रहे हैं।
+                        </p>
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/agriculture1.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/agriculture2.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/agriculture3.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/agriculture4.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/agriculture5.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/agriculture6.jpg" alt="" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php include('footer.php') ?>
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- JS Files -->
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+    </body>
+
+</html>
