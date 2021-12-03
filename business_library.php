964e2a4d8b9e7432288012b18a40b1c652087efc
Andrewnomaan <andrew.nomaan@gmail.com>
2021-12-03 18:14:59 +0530

    business_library

---
+++ business_library.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>व्यापार पहल लाईब्रेरी - समाजवादी पार्टी, बिजनौर</title>
+    <?php include 'header.php'; 
+  $assets='assets/img/downloads/';
+?>
+    <main id="main">
+        <section id="gallery" class="section-bg">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>व्यापार पहल लाईब्रेरी</h2>
+                </div>
+
+               
+                <div class="row">
+                <div class="col-md-4 pt-4">
+                        <img src="assets/img/business1.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/business2.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/business3.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/business4.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/business5.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/business6.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/business7.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/business8.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/business9.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/business10.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/busin.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/business12.jpg" alt="" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php include('footer.php') ?>
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- JS Files -->
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+    </body>
+
+</html>
