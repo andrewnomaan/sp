739c8600726ed91b873de23d089d108082a40829
Andrewnomaan <andrew.nomaan@gmail.com>
2021-12-03 18:15:16 +0530

    business

---
+++ business.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>व्यापार पहल - समाजवादी पार्टी, बिजनौर</title>
+    <?php include 'header.php'; 
+?>
+    <main id="main">
+        <section id="" class="section-bg">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>व्यापार पहल</h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-5">
+                        <img src="assets/img/business.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-lg-7 pt-3">
+                        <p>
+                            उत्तर प्रदेश में उद्यमी अब सरकार समर्थित कई योजनाओं का लाभ उठा सकते हैं। समाजवादी पार्टी
+                            उद्यमियों को उनकी पहल के लिए उत्तर प्रदेश को घर के रूप में चुनने के लिए प्रोत्साहित करती है
+                            और उनके लिए एक अनुकूल वातावरण सुनिश्चित करने के लिए कई नीतियां बनाई हैं। बेहतर बुनियादी
+                            ढांचे के लिए धन, अनुदान और ऋण तक आसान पहुंच प्रदान करने से; व्यवसाय शुरू करने के लिए उत्तर
+                            प्रदेश नया पसंदीदा स्थान है।
+                        </p>
+                        <p>
+
+                            देश में विभिन्न क्षेत्रों में कुछ सबसे बड़ी कंपनियां, जो हाल ही में राज्य में निवेश के बारे
+                            में आशंकित थीं, ने कुल रु। का निवेश करने का फैसला किया। राज्य में आधार स्थापित करने के लिए
+                            7,500 करोड़ रुपये। यह समाजवादी पार्टी की पहल के कारण ही संभव हो पाया है। सैमसंग इंडिया
+                            इलेक्ट्रॉनिक्स प्राइवेट लिमिटेड, नोएडा; रिलायंस सीमेंट कंपनी प्रा। लिमिटेड, इंडो-गल्फ
+                            फर्टिलाइजर, जगदीशपुर; गुजरात को-ऑपरेटिव मिल्क मार्केटिंग फेडरेशन (जीसीएमएमएफ), एचसीएल
+                            टेक्नोलॉजीज लिमिटेड और टाटा समूह उन कंपनियों के नाम हैं, जिन्हें उत्तर प्रदेश में अपना घर
+                            मिला है।
+                        </p>
+                        <p>
+                            विभिन्न योजनाएं उन उद्यमियों को अनुदान प्रदान कर रही हैं जो अनुसंधान और विकास और बुनियादी
+                            ढांचे
+
+                        </p>
+
+                    </div>
+                    <div class="col-12 pt-2">
+                        <p>
+                            के विकास में संलग्न हैं; अंतिम उत्पाद की लागत सुनिश्चित करना विकास की लागत से कम होगा।
+                            उत्तर प्रदेश में समाजवादी पार्टी ने बड़े बुनियादी ढांचे में बदलाव लाए हैं जिससे लोगों के लिए
+                            राज्य में यात्रा करना, आयात करना, निर्यात करना और निवेश करना आसान हो गया है।
+                        </p>
+                        <b>
+                            रतन टाटा ने 'नए उत्तर प्रदेश' पर समाजवादी पार्टी की तारीफ करते हुए कहा, 'मैं यूपी के लिए
+                            अपना दिल हार रहा हूं।
+                        </b>
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/business1.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/business2.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/business3.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/business4.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/business5.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/business6.jpg" alt="" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php include('footer.php') ?>
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- JS Files -->
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+    </body>
+
+</html>
