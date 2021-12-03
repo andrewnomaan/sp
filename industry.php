f90f7a5dae84df82eeca1624c27cb13f565a6244
Andrewnomaan <andrew.nomaan@gmail.com>
2021-12-03 18:12:04 +0530

    industry

---
+++ industry.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>औद्योगिक पहल - समाजवादी पार्टी, बिजनौर</title>
+    <?php include 'header.php'; 
+?>
+    <main id="main">
+        <section id="" class="section-bg">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>औद्योगिक पहल</h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-5">
+                        <img src="assets/img/industry.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-lg-7 pt-3">
+                        <p>
+                            कृषि तकनीकों के आधुनिकीकरण, विज्ञान और प्रौद्योगिकी को नियोजित करके नए नवाचारों को विकसित
+                            करने, नई पूंजी निर्माण और समग्र आर्थिक विकास के लिए औद्योगिक विकास महत्वपूर्ण है।
+                        </p>
+                        <p>
+                            उत्तर प्रदेश कुशल श्रमिकों के एक बड़े पूल के साथ भारत में सबसे अधिक आबादी वाला राज्य है और
+                            साथ ही साथ देश में लगभग 200,000,000 लोगों के साथ सबसे बड़ा उपभोक्ता आधार है। 2014-15 में,
+                            राज्य का सकल घरेलू उत्पाद (सकल घरेलू उत्पाद) रु। 16,100 करोड़ से अधिक हो गया है
+                        </p>
+                        <p>
+                            पिछले दशक में 100 गुना। राज्य का बुनियादी ढांचा, प्रोत्साहन और संसाधन औद्योगिक विकास के लिए
+                            उपयुक्त हैं और समाजवादी पार्टी ने राज्य के लाभ के लिए इन शर्तों का लाभ उठाया है। "उद्योग
+                            बंधु", जिसका अर्थ है 'उद्योग का मित्र' एक ऐसा संगठन है जो विभिन्न औद्योगिक क्षेत्रों में
+                            निवेश की सुविधा के लिए समर्पित है।
+                        </p>
+                        <p>
+                            समाजवादी पार्टी ने कई स्थानों पर कपड़ा, कृषि, चमड़ा और ईपीआईपी उद्योगों की शुरुआत की है और
+                            इसे अन्य शहरों से जोड़ने के लिए राज्य को एक ढांचागत बदलाव दिया है। इससे सभी क्षेत्रों में
+                            औद्योगिक केंद्रों और पीपीपी परियोजनाओं की संख्या में उल्लेखनीय वृद्धि हुई है।
+                        </p>
+
+                    </div>
+
+                </div>
+                <div class="row mt-5">
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/industry1.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/industry2.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/industry3.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/industry4.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/industry5.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/industry6.jpg" alt="" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php include('footer.php') ?>
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- JS Files -->
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+    </body>
+
+</html>
