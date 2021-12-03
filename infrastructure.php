0bd7ff76ba9af0f58cecd603a40e743ae443451e
Andrewnomaan <andrew.nomaan@gmail.com>
2021-12-03 18:12:28 +0530

    infrastructure

---
+++ infrastructure.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>बुनियादी ढांचा - समाजवादी पार्टी, बिजनौर</title>
+    <?php include 'header.php'; 
+?>
+    <main id="main">
+        <section id="" class="section-bg">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>बुनियादी ढांचा पहल</h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-5">
+                        <img src="assets/img/infrastructure.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-lg-7 pt-3">
+                        <p>
+                            समाजवादी पार्टी भौतिक बुनियादी ढांचे के वितरण, रखरखाव और संचालन में सुधार पर केंद्रित है।
+                            पार्टी ने निजी, सार्वजनिक और गैर-लाभकारी क्षेत्रों में भागीदारों के साथ कई सहयोगों के
+                            साथ-साथ इस महत्वपूर्ण पहलू पर पूरी तरह ध्यान केंद्रित करने के लिए शुरू की गई विभिन्न नीतियों
+                            के माध्यम से इसे पूरा किया है।
+                        </p>
+                        <p>
+                            उत्तर प्रदेश औद्योगिक एवं आधारभूत संरचना निवेश नीति 2012 के तहत विभिन्न नीतियां एवं योजनाएं
+                            बनाई गई हैं। जिसके अनुसार निवेशकों को एस्कॉर्ट सेवाएं और सिंगल विंडो क्लियरिंग दी जाएगी।
+                            राज्य में रोजगार दर बढ़ाने के लिए नई ईपीएफ प्रतिपूर्ति योजना शुरू की गई है। पूर्वी और मध्य
+                            उत्तर प्रदेश में स्थापित होने वाली नई औद्योगिक इकाइयों पर शुल्क स्टाम्प से शत-प्रतिशत छूट
+                            होगी, जिससे पूरे राज्य का विकास सुनिश्चित होगा।
+
+                            समाजवादी पार्टी ने घोषणा की है कि वह आईटी, कृषि प्रसंस्करण इकाइयों, जैव प्रौद्योगिकी, और
+                            बुनियादी ढांचा परियोजनाओं जैसे सड़कों, ट्रांस-शिपमेंट सेंटर, बिजली, गोदाम, थोक और कोल्ड
+                            स्टोरेज जैसे क्षेत्रों के लिए स्टांप शुल्क से कुल छूट प्रदान करेगी। 2016-17 के बजट में लगभग
+                            रु. ढांचागत विकास के लिए 1,00,000 करोड़ रुपये प्रस्तावित किए गए हैं।
+                        </p>
+                    </div>
+                    <div class="col-12 pt-4">
+                        <p>
+                            सौर ऊर्जा उद्योग अब राज्य में आसान स्थापना के लिए एक आकर्षक उपकरण प्रदान करते हैं। पूंजीगत
+                            ब्याज सब्सिडी योजना से लेकर प्रवेश कर में छूट तक ने राज्य के बुनियादी ढांचे को मजबूत करते
+                            हुए उत्तर प्रदेश में कई बड़े नामों को लुभाया है।
+                        </p>
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/infrastructure1.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/infrastructure2.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/infrastructure3.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/infrastructure4.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/infrastructure5.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-4 pt-4">
+                        <img src="assets/img/infrastructure6.jpg" alt="" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php include('footer.php') ?>
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- JS Files -->
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+    </body>
+
+</html>
