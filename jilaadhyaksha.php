a6edf4ae1d041b8d0ea82835ad21a908e6eac7a3
Andrewnomaan <andrew.nomaan@gmail.com>
2021-12-04 18:12:37 +0530

    jilaadhyaksha

---
+++ jilaadhyaksha.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>पार्टी नेता - समाजवादी पार्टी, बिजनौर</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/logo.JPG" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include('header.php') ?>
+
+    <main id="main">
+
+        <section id="president" class="president">
+            <div class="container">
+                <div class="section-title">
+                    <h2>जिला अध्यक्ष</h2>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-md-6">
+                        <div class="card shadow rounded akhilesh-about">
+                            <img src="assets/img/rashidhusain.jpg" class="img-fluid" alt="">
+                            <div class="card-body">
+                                <h3>राशिद हुसैन</h3>
+                                <span>जिला अध्यक्ष &middot; समाजवादी पार्टी, बिजनौर</span>
+                                <!-- <p>
+                            1 जुलाई 1973 को सैफई में मुलायम सिंह यादव और मालती देवी के घर जन्मे अखिलेश यादव ने राजस्थान के धौलपुर मिलिट्री स्कूल में प्रारंभिक शिक्षा प्राप्त की। इसके बाद उन्होंने क्रमशः मैसूर विश्वविद्यालय और सिडनी विश्वविद्यालय, ऑस्ट्रेलिया से सिविल पर्यावरण इंजीनियरिंग में स्नातक और मास्टर डिग्री प्राप्त की।
+                        </p> -->
+                                <p></p>
+                                <a href="https://www.facebook.com/" class="btn-know-more me-2"><i class="bi bi-facebook facebook"></i></a>
+                                <a href="https://twitter.com/" class="btn-know-more"><i class="bi bi-twitter twitter"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <!-- End #main -->
+
+    <?php include('footer.php') ?>
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- JS Files -->
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
